0b8fc8cc1ba5ce9db422c281b886c9caf488f64c
GregNYT <gregory.joshua@nytimes.com>
2018-07-23 15:15:46 -0400

    Initial Commit

---
+++ Paper2Video/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Paper2Video
+//
+//  Created by Joshua, Gregory on 7/23/18.
+//  Copyright © 2018 NOCDIB, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
